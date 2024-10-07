0c0b2e643e6f7f321f1c6d5c1eeaedd8d0b6a869
Geetansh431 <geetanshg2@gmail.com>
2024-10-08 00:03:08 +0530

    1.1.0

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -2,8 +2,27 @@ package src;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
 
 
-public class FlappyBird {
-    
+public class FlappyBird extends JPanel {
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    //image
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.blue);
+
+        // load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+    }
 }
