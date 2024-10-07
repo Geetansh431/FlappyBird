d02a0bea4fd6467fbdf0d60e9b0644fa4b6af6a1
Geetansh431 <geetanshg2@gmail.com>
2024-10-08 00:11:01 +0530

    added background images

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -20,9 +20,23 @@ public class FlappyBird extends JPanel {
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+        // setBackground(Color.blue);
 
         // load images
         backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //background
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+        
     }
 }
