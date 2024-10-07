a016e6e82b649eab41ecf42afa10d100d4ba34a0
Geetansh431 <geetanshg2@gmail.com>
2024-10-07 23:43:05 +0530

    1.0.0

---
+++ src/App.java
@@ -0,0 +1,19 @@
+package src;
+import java.util.*;
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+    }
+}
